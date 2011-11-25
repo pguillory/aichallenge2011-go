f23654abe4c83bb1f4ed34e236afc65fccbca054
Preston Guillory <pguillory@gmail.com>
2011-11-24 23:00:06 -0800

    The hell.

--- Scent.go
+++ Scent.go
@@ -5,7 +5,7 @@ import "fmt"
 type ScentChannel struct {
     pathways [DIRECTIONS]*float32
     multiplier float32
-    additive float32
+    //additive float32
 }
 
 type Scent struct {
@@ -14,7 +14,7 @@ type Scent struct {
     terrain *Terrain
     distanceToEnemy, distanceToFriendlyHill *TravelDistance
     mystery *Mystery
-    configureChannel func(*Scent, Point, *ScentChannel)
+    configureChannel func(*Scent, Point, *ScentChannel) float32
     adjacentWater *AdjacentWater
     value [MAX_ROWS][MAX_COLS]float32
     channels [MAX_ROWS][MAX_COLS]ScentChannel
@@ -28,7 +28,7 @@ func NewBattleScent(terrain *Terrain, distanceToEnemy *TravelDistance, distanceT
     return NewScent(terrain, distanceToEnemy, distanceToFriendlyHill, mystery, ConfigureBattleScentChannel)
 }
 
-func NewScent(terrain *Terrain, distanceToEnemy *TravelDistance, distanceToFriendlyHill *TravelDistance, mystery *Mystery, configureChannel func(*Scent, Point, *ScentChannel)) *Scent {
+func NewScent(terrain *Terrain, distanceToEnemy *TravelDistance, distanceToFriendlyHill *TravelDistance, mystery *Mystery, configureChannel func(*Scent, Point, *ScentChannel) float32) *Scent {
     this := new(Scent)
     this.terrain = terrain
     this.distanceToEnemy = distanceToEnemy
@@ -41,6 +41,14 @@ func NewScent(terrain *Terrain, distanceToEnemy *TravelDistance, distanceToFrien
     return this
 }
 
+func (this *Scent) Emanate(p Point, value float32) {
+    this.value[p.row][p.col] += value
+}
+
+func (this *Scent) Absorb(p Point) {
+    this.value[p.row][p.col] = 0
+}
+
 func (this *Scent) Calculate() {
     this.distanceToEnemy.Calculate()
     this.distanceToFriendlyHill.Calculate()
@@ -51,17 +59,6 @@ func (this *Scent) Calculate() {
     }
     startTime := now()
 
-    this.BuildChannels()
-
-    for i := 0; i < 100; i++ {
-        this.Spread()
-    }
-
-    this.time = now() - startTime
-    this.turn = turn
-}
-
-func (this *Scent) BuildChannels() {
     ForEachPoint(func(p Point) {
         channel := &this.channels[p.row][p.col]
         channel.pathways[0] = &this.value[(p.row - 1 + rows) % rows][(p.col           )       ]
@@ -71,26 +68,32 @@ func (this *Scent) BuildChannels() {
         channel.pathways[4] = &this.value[(p.row + 1       ) % rows][(p.col           )       ]
 
         channel.multiplier = 1.0 / (5.0 - float32(this.adjacentWater.At(p)))
-        //channel.multiplier = 0.2
-        channel.additive = 0
+        //channel.additive = 0
 
-        this.configureChannel(this, p, channel)
+        this.value[p.row][p.col] *= 0.1
+        this.value[p.row][p.col] += this.configureChannel(this, p, channel)
     })
+
+    for i := 0; i < 100; i++ {
+        this.Spread()
+    }
+
+    this.time = now() - startTime
+    this.turn = turn
 }
 
-func ConfigureForageScentChannel(this *Scent, p Point, channel *ScentChannel) {
-    this.value[p.row][p.col] *= 1e-1
-
-    //channel.multiplier *= 0.95
-
+func ConfigureForageScentChannel(this *Scent, p Point, channel *ScentChannel) float32 {
     s := this.terrain.At(p)
 
     switch {
     case s.HasFood():
-        channel.additive = 0.1
+        return 1e15
+        //channel.additive = 0.1
     case s.HasHill():
         if s.IsEnemy() {
-            channel.additive = 1.0
+            return 1e20
+            //this.value[p.row][p.col] += 1e20
+            //channel.additive = 1.0
         } else {
             channel.multiplier = 0.0
         }
@@ -98,9 +101,13 @@ func ConfigureForageScentChannel(this *Scent, p Point, channel *ScentChannel) {
         if s.IsEnemy() {
             switch {
             case this.distanceToFriendlyHill.At(p) < 20:
-                channel.additive = 1.0 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
+                //channel.additive = 1.0 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
+                //this.value[p.row][p.col] += 1.0 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
+                return 1e20 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
             case this.terrain.VisibleFriendliesAt(p) >= 2:
-                channel.additive = 0.01
+                //channel.additive = 0.01
+                //this.value[p.row][p.col] += 0.01
+                return 1e12
             }
         } else {
             channel.multiplier = 0.0
@@ -108,12 +115,16 @@ func ConfigureForageScentChannel(this *Scent, p Point, channel *ScentChannel) {
     case s.HasWater():
         channel.multiplier = 0.0
     default:
-        channel.additive = this.mystery.At(p) * 0.002
+        //channel.additive = this.mystery.At(p) * 0.002
+        //this.value[p.row][p.col] += this.mystery.At(p) * 0.002
+        return this.mystery.At(p) * 1e10
     }
+
+    return 0.0
 }
 
-func ConfigureBattleScentChannel(this *Scent, p Point, channel *ScentChannel) {
-    this.value[p.row][p.col] *= 1e-1
+func ConfigureBattleScentChannel(this *Scent, p Point, channel *ScentChannel) float32 {
+    //this.value[p.row][p.col] *= 1e-1
 
     //channel.multiplier *= 0.99
 
@@ -122,7 +133,8 @@ func ConfigureBattleScentChannel(this *Scent, p Point, channel *ScentChannel) {
     switch {
     case s.HasHill():
         if s.IsEnemy() {
-            channel.additive = 1.0
+            //channel.additive = 1.0
+            return 1e20
         } else {
             channel.multiplier = 0.0
         }
@@ -130,9 +142,11 @@ func ConfigureBattleScentChannel(this *Scent, p Point, channel *ScentChannel) {
         if s.IsEnemy() {
             switch {
             case this.distanceToFriendlyHill.At(p) < 20:
-                channel.additive = 1.0 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
+                //channel.additive = 1.0 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
+                return 1e20 * float32(20 - this.distanceToFriendlyHill.At(p)) / 20.0
             default:
-                channel.additive = 0.01
+                //channel.additive = 0.01
+                return 1e12
             }
         } else {
             channel.multiplier *= 1e-2
@@ -140,10 +154,15 @@ func ConfigureBattleScentChannel(this *Scent, p Point, channel *ScentChannel) {
     case s.HasWater():
         channel.multiplier = 0.0
     default:
+        //if this.mystery.At(p) > STARTING_MYSTERY {
+        //    channel.additive = this.mystery.At(p) * 0.002
+        //}
         if this.mystery.At(p) > STARTING_MYSTERY {
-            channel.additive = this.mystery.At(p) * 0.002
+            return this.mystery.At(p) * 1e10
         }
     }
+
+    return 0.0
 }
 
 func (this *Scent) Spread() {
@@ -160,7 +179,7 @@ func (this *Scent) Spread() {
                 *channel.pathways[3] +
                 *channel.pathways[4]
 
-            newValue[p.row][p.col] = v * channel.multiplier + channel.additive //* 1e20
+            newValue[p.row][p.col] = v * channel.multiplier //+ channel.additive //* 1e20
         }
     }
 
@@ -182,21 +201,21 @@ func (this *Scent) String() string {
             switch {
             case this.At(p) < 0.0: return '-'
             case this.At(p) == 0.0: return '0'
-            case this.At(p) <= 1e0: return '1'
-            case this.At(p) <= 1e1: return '2'
-            case this.At(p) <= 1e2: return '3'
-            case this.At(p) <= 1e3: return '4'
-            case this.At(p) <= 1e4: return '5'
-            case this.At(p) <= 1e5: return '6'
-            case this.At(p) <= 1e6: return '7'
-            case this.At(p) <= 1e7: return '8'
-            case this.At(p) <= 1e8: return '9'
-            case this.At(p) <= 1e9: return 'a'
-            case this.At(p) <= 1e10: return 'b'
-            case this.At(p) <= 1e11: return 'c'
-            case this.At(p) <= 1e12: return 'd'
-            case this.At(p) <= 1e13: return 'e'
-            case this.At(p) <= 1e14: return 'f'
+            case this.At(p) <= 1e2: return '1'
+            case this.At(p) <= 1e4: return '2'
+            case this.At(p) <= 1e6: return '3'
+            case this.At(p) <= 1e8: return '4'
+            case this.At(p) <= 1e10: return '5'
+            case this.At(p) <= 1e12: return '6'
+            case this.At(p) <= 1e14: return '7'
+            case this.At(p) <= 1e16: return '8'
+            case this.At(p) <= 1e18: return '9'
+            case this.At(p) <= 1e20: return 'a'
+            case this.At(p) <= 1e22: return 'b'
+            case this.At(p) <= 1e24: return 'c'
+            case this.At(p) <= 1e26: return 'd'
+            case this.At(p) <= 1e28: return 'e'
+            case this.At(p) <= 1e30: return 'f'
             }
             return '+'
         case square.HasWater():
